2e49ad031afe0ae82e8bea0a2a627654fd6a4461
Kate <159571202+turchynska@users.noreply.github.com>
2024-06-21 13:28:07 -0400

    homeWork9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,63 @@
 const formData = {
-email: "",
-massage: ""
+    email: "", message: "",
+
+    setData(newEmail, newMessage) {
+        this.email = newEmail;
+        this.message = newMessage;
+}}
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener("input", formInput);
+function formInput(event) {
+    switch (event.target.name) {
+        case "email":
+            formData.email = event.target.value.trim();
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+            break;
+        case "message":
+            formData.message = event.target.value.trim()
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+            break;
+    }
 }
+function getValueOfLocalStorage() {
+    const localData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (localData !== null) {
+        formData.setData(localData.email, localData.message)
+        form.email.value = localData.email;
+        form.message.value = localData.message;
+      
+       
+        
+    }
+}
+getValueOfLocalStorage();
+
+
+
+form.addEventListener("submit", formSubmit)
+function formSubmit(event) {
+    event.preventDefault()
+    if (
+        formData.email === "" || formData.message === "") {
+        return alert("Fill please all fields")
+        
+       
+    }
+    console.table(formData);
+    formData.setData("", "");
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+       
+}
+function onSubmit(event) {
+  event.preventDefault();
+  if (formData.email === "" || formData.message === "") {
+    return alert("Fill please all fields");
+  }
+  console.table(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  formMessage.reset();
+}
+getValueOfLocalStorage();
